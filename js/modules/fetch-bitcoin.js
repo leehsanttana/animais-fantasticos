d850c7b0af6aeaad29ec85721869d9ff45bcdbb8
leehsanttana <leonamsanttana@gmail.com>
2021-02-18 10:46:32 -0300

    primeiro commit

---
+++ js/modules/fetch-bitcoin.js
@@ -0,0 +1,8 @@
+export default function initFetchBitcoin() {
+  fetch('https://blockchain.info/ticker').then((response) => response.json()).then((bitcoin) => {
+    const btnPreco = document.querySelector('.btc-preco');
+    btnPreco.innerText = (1000 / bitcoin.BRL.sell).toFixed(4);
+  }).catch((erro) => {
+    console.log(Error(erro));
+  });
+}
---
+++ js/modules/teste.js
@@ -0,0 +1,7 @@
+export function teste1() {
+  console.log('Isso é um teste');
+}
+
+export function teste2() {
+  console.log('Isso é um uasuahsaushu');
+}
